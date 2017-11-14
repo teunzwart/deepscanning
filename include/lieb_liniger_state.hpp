ddda65be764db5a9a65a7f54c3e818d6d0d4d1b8
Teun Zwart <teunzwart@gmail.com>
2017-11-14 17:02:22 +0100

    Write construction of ground state Bethe numbers

--- include/lieb_liniger_state.hpp
+++ include/lieb_liniger_state.hpp
@@ -1,8 +1,22 @@
+#ifndef LIEB_LINIGER_STATE_H
+#define LIEB_LINIGER_STATE_H
+
+#include <vector>
+
 class lieb_liniger_state
 {
-    public:
-        double c;
-        double L;
-        double N;
-        lieb_liniger_state(double new_c, double new_L, double new_N);
+public:
+    double c;
+    double L;
+    int N;
+    std::vector<double> Is;
+    std::vector<double> lambdas;
+    
+    lieb_liniger_state(double new_c, double new_L, int new_N);
+    lieb_liniger_state(double new_c, double new_L, int new_N,
+                       std::vector<double> new_Is);
+
+    void generate_gs_bethe_numbers();
 };
+
+#endif // LIEB_LINIGER_STATE_H
--- src/lieb_liniger_state.cpp
+++ src/lieb_liniger_state.cpp
@@ -1,18 +1,44 @@
 #include "lieb_liniger_state.hpp"
 
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 
-lieb_liniger_state::lieb_liniger_state(double new_c, double new_L, double new_N)
+lieb_liniger_state::lieb_liniger_state(double new_c, double new_L,
+                                       int new_N)
     : c(new_c),
       L(new_L),
-      N(new_N)
+      N(new_N),
+      Is(N)
 {
-    std::cout << "Hello" << std::endl;
+    std::cout << "Hello, this is the ground state." << std::endl;
+    generate_gs_bethe_numbers();
+    for (auto i: Is) {
+        std::cout << i << std::endl;
+    }
 }
 
+lieb_liniger_state::lieb_liniger_state(double new_c, double new_L,
+                                       int new_N,
+                                       std::vector<double> new_Is)
+    : c(new_c),
+      L(new_L),
+      N(new_N),
+      Is(new_Is)
+{
+    std::cout << "Hello, this is an excited state." << std::endl;
+}
 
-int main() {
-    lieb_liniger_state test(1, 100, 100);
+/**
+ * Generate the Bethe numbers of the N particle Lieb-Liniger ground state.
+ */
+void lieb_liniger_state::generate_gs_bethe_numbers() {
+    if (N % 2 == 0) {
+        std::generate(Is.begin(), Is.end(),
+                      [this, n=0]() mutable {return -N/2 + 0.5 + n++;});
+    } else {
+        std::generate(Is.begin(), Is.end(),
+                      [this, n=0]() mutable {return -N/2 + n++;});
+    }
 }
