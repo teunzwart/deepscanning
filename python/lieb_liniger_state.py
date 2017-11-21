ba06fcd28a86642782554bbb23e357192814c883
Teun Zwart <teunzwart@gmail.com>
2017-11-21 16:06:03 +0100

    Add Python version for random Bethe number generation

--- python/lieb_liniger_state.py
+++ python/lieb_liniger_state.py
@@ -6,7 +6,7 @@ class lieb_liniger_state:
         self.c = c
         self.L = L
         self.N = N
-        if bethe_numbers:
+        if bethe_numbers is not None:
             self.Is = bethe_numbers
         else:
             self.Is = self.generate_gs_bethe_numbers()
@@ -35,10 +35,8 @@ class lieb_liniger_state:
                 else:
                     self.gaudin_matrix[j, k] = -self.kernel(self.lambdas[j] - self.lambdas[k], self.c)
 
-    def calculate_rapidities_newton(self):
+    def calculate_rapidities_newton(self, printing=False):
         """Calculate the rapidities using a multidimensional Newton method."""
-        self.lambdas = 2 * np.pi / self.L * self.Is
-
         for no_of_iterations in range(20):
             rhs_bethe_equations = np.zeros(self.N)
             for j in range(self.N):
@@ -53,15 +51,40 @@ class lieb_liniger_state:
             for i in range(self.N):
                 diff_square += delta_lambda[i]**2 / (self.lambdas[i]**2 + 10**-6)
             diff_square /= self.N
+            if printing:
+                print(diff_square)
 
             for i in range(self.N):
                 self.lambdas[i] += delta_lambda[i]
 
-            if diff_square < 10**-30:
-                break
+            if diff_square < 10**-14:
+                return no_of_iterations
+            # if printing:
+            #     print(self.lambdas)
+
+        return no_of_iterations
+
+
+def generate_gs_bethe_numbers(N):
+    bethe_numbers = np.full(N, 10.**7, dtype=np.float)
+    no_of_unique_entries = 0
+    while no_of_unique_entries < N:
+        random_number = np.round(np.random.normal(0, N))
+        if N % 2 == 0:
+            random_number += np.sign(np.random.uniform(-1, 1)) * 0.5
+
+        if (np.abs(bethe_numbers - random_number) < 10e-4).any():
+            continue
+        else:
+            bethe_numbers[no_of_unique_entries] = random_number
+            no_of_unique_entries += 1
+
+    return np.sort(bethe_numbers)
 
 
 if __name__ == "__main__":
     llstate = lieb_liniger_state(1, 100, 5)
+    llstate.lambdas = 2 * np.pi / llstate.L * llstate.Is
     llstate.calculate_rapidities_newton()
     print(llstate.lambdas)
+    print(generate_gs_bethe_numbers(6))
