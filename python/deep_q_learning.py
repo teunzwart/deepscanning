7edf3a632ee88dd92a8f99c9a99eac5e2e1cca08
Teun Zwart <teunzwart@gmail.com>
2018-02-19 11:08:35 +0100

    Implement skipping of already visited states

--- python/deep_q_learning.py
+++ python/deep_q_learning.py
@@ -16,7 +16,7 @@ from utils import map_to_entire_space, map_to_bethe_numbers, get_valid_random_ac
 def neural_net(N_world):
     model = Sequential()
     model.add(Dense(units=N_world, activation='relu', kernel_initializer='lecun_uniform', input_dim=N_world))
-    # model.add(Dense(units=int(N_world**1.5), kernel_initializer='lecun_uniform', activation='softmax'))
+    model.add(Dense(units=int(N_world**1.5), kernel_initializer='lecun_uniform', activation='softmax'))
     model.add(Dense(units=N_world**2, kernel_initializer='lecun_uniform', activation='relu'))
     model.compile(loss='mse', optimizer=RMSprop())
     model.summary()
@@ -24,16 +24,11 @@ def neural_net(N_world):
 
 
 def change_state(state, action):
-    # print(state, action)
-    # print("0", state[action[0]])
-    # print("1", state[action[1]])
     try:
         state[action[0]] -= 1
         state[action[1]] += 1
     except (TypeError, IndexError):
-        # print(state, action)
         sys.exit()
-    # print()
     return state
 
 
@@ -45,6 +40,8 @@ def q_learning(N_world, max_I, L, N, gamma=0.9, epochs=100, epsilon=1):
     model = neural_net(N_world)
     rstate = lls.lieb_liniger_state(1, L, N)
     rstate.calculate_all()
+    highest = 0
+    sums = []
     for t in range(epochs):
         print(f"Epoch {t}")
         dsf_data = {}
@@ -55,30 +52,24 @@ def q_learning(N_world, max_I, L, N, gamma=0.9, epochs=100, epsilon=1):
             Q = model.predict(map_to_entire_space(lstate.Is, max_I).reshape(1, -1))
             # print(Q.shape)
             if np.random.random() < epsilon:
-                # print("random")
-                action = get_valid_random_action(map_to_entire_space(lstate.Is, max_I), N_world)
+                while True:
+                    action = get_valid_random_action(map_to_entire_space(lstate.Is, max_I), N_world)
+                    new_state = change_state(map_to_entire_space(lstate.Is, max_I), action)
+                    if list(new_state) not in previously_visited:
+                        break
             else:
-                # print("not random")
                 for a in array_to_list(Q.reshape(N_world, N_world)):
                     if is_valid_action(map_to_entire_space(lstate.Is, max_I), a, N_world):
                         # print("a", a)
                         action = a[1]
+                        print(action)
                         new_state = change_state(map_to_entire_space(lstate.Is, max_I), action)
-                        break
-                # action = np.unravel_index(np.argmax(Q), (N_world, N_world))
-            # print(action, "\n")
-            new_state = change_state(map_to_entire_space(lstate.Is, max_I), action)
+                        if list(new_state) not in previously_visited:
+                            break
+            previously_visited.append(list(new_state))
             new_lstate = lls.lieb_liniger_state(1, N, L, map_to_bethe_numbers(new_state, max_I))
-            try:
-                new_lstate.calculate_all()
-            except (IndexError, ValueError) as e:
-                print(e)
-                print(lstate)
-                print(new_state)
-                print(action)
-                sys.exit()
+            new_lstate.calculate_all()
             reward = get_reward(new_lstate, rstate)
-            # print("reward", reward)
 
             new_Q = model.predict(new_state.reshape(1, -1))
             new_max_Q = np.argmax(new_Q)
@@ -87,7 +78,6 @@ def q_learning(N_world, max_I, L, N, gamma=0.9, epochs=100, epsilon=1):
             y[:] = Q[:]
 
             update = reward + gamma * new_max_Q
-            # print(np.ravel_multi_index(action, (N_world, N_world)))
             y[np.ravel_multi_index(action, (N_world, N_world))] = update
 
             # print(map_to_entire_space(lstate.Is, max_I).shape)
@@ -107,10 +97,16 @@ def q_learning(N_world, max_I, L, N, gamma=0.9, epochs=100, epsilon=1):
             else:
                 dsf_data[lstate.integer_momentum] = [lstate]
 
-        print(compute_average_sumrule(dsf_data, rstate.energy, N, L, False))
+        ave_sum_rule = compute_average_sumrule(dsf_data, rstate.energy, N, L, False)
+        sums.append(ave_sum_rule)
+        print(ave_sum_rule)
+        if ave_sum_rule > highest:
+            highest = ave_sum_rule
 
         if epsilon > 0.1:
             epsilon -= 1 / epochs
+    print("highest", highest)
+    print(sums)
 
 
 if __name__ == "__main__":
