d9ae3d991d00e334cb6bd133a5e154bdf605df83
Teun Zwart <teunzwart@gmail.com>
2018-03-01 14:07:34 +0100

    Return best dsf_data

--- python/deep_q_learning.py
+++ python/deep_q_learning.py
@@ -22,35 +22,48 @@ def neural_net(N_world):
     model.summary()
     return model
 
+    # model = Sequential()
+    # model.add(Dense(N_world*5, kernel_initializer='lecun_uniform', input_shape=(N_world,), activation="relu"))
+    # model.add(Dense(N_world*5, kernel_initializer='lecun_uniform', activation="relu"))
+    # model.add(Dense(N_world**2, kernel_initializer='lecun_uniform', activation="tanh"))
+    # model.compile(loss='mse', optimizer=RMSprop())
+    # model.summary()
+    # return model
+
 
 def change_state(state, action):
-    try:
-        state[action[0]] -= 1
-        state[action[1]] += 1
-    except (TypeError, IndexError):
-        sys.exit()
+    state[action[0]] -= 1
+    state[action[1]] += 1
     return state
 
 
 def get_reward(lstate, rstate):
-    return np.abs(rff.calculate_normalized_form_factor(lstate, rstate))**2
+    return (lstate.energy - rstate.energy) * np.abs(rff.calculate_normalized_form_factor(lstate, rstate))**2
 
 
-def q_learning(N_world, max_I, L, N, gamma=0.9, epochs=100, epsilon=1):
+def distance_to_rstate(lstate, rstate):
+    """Calculate the 'distance' between lstate and rstate."""
+    distance = 0
+    for i, n in enumerate(np.argwhere(np.array(rstate) == 1)):
+        distance += np.abs(n[0] - np.where(np.array(lstate) == 1)[0][i])
+    return distance
+
+
+def q_learning(N_world, max_I, L, N, gamma=0.975, epochs=100, epsilon=1, no_of_steps=100):
     model = neural_net(N_world)
     rstate = lls.lieb_liniger_state(1, L, N)
     rstate.calculate_all()
     highest = 0
     sums = []
+    best_dsf = None
     for t in range(epochs):
         print(f"Epoch {t}")
         dsf_data = {}
         previously_visited = []
         lstate = lls.lieb_liniger_state(1, L, N)
         lstate.calculate_all()
-        for n in range(100):
+        for n in range(no_of_steps + 1):
             Q = model.predict(map_to_entire_space(lstate.Is, max_I).reshape(1, -1))
-            # print(Q.shape)
             if np.random.random() < epsilon:
                 while True:
                     action = get_valid_random_action(map_to_entire_space(lstate.Is, max_I), N_world)
@@ -60,9 +73,7 @@ def q_learning(N_world, max_I, L, N, gamma=0.9, epochs=100, epsilon=1):
             else:
                 for a in array_to_list(Q.reshape(N_world, N_world)):
                     if is_valid_action(map_to_entire_space(lstate.Is, max_I), a, N_world):
-                        # print("a", a)
                         action = a[1]
-                        print(action)
                         new_state = change_state(map_to_entire_space(lstate.Is, max_I), action)
                         if list(new_state) not in previously_visited:
                             break
@@ -102,11 +113,13 @@ def q_learning(N_world, max_I, L, N, gamma=0.9, epochs=100, epsilon=1):
         print(ave_sum_rule)
         if ave_sum_rule > highest:
             highest = ave_sum_rule
+            best_dsf = dsf_data
 
         if epsilon > 0.1:
             epsilon -= 1 / epochs
     print("highest", highest)
     print(sums)
+    return best_dsf
 
 
 if __name__ == "__main__":
